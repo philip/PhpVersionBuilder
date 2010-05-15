9a108c3bc7fc696900c1614d3842ab71ee9ca4d9
philip <philip@roshambo.org>
2010-05-14 18:52:12 -0700

    Implemented colour enhanced HTML output mode, when testing/executing PHP code.

--- execute_php.php
+++ execute_php.php
@@ -3,10 +3,15 @@
 require './inc/config.php';
 require './inc/pvb-functions.php';
 
+// FIXME: Use getopt() I suppose
 if ($_SERVER['argc'] === 1) {
 	echo 'Error: Pass in a PHP script to execute.' . PHP_EOL;
-	echo 'Usage: ' . $_SERVER['argv'][0] . ' phpfile.php' . PHP_EOL;
+	echo 'Usage: ' . $_SERVER['argv'][0] . ' phpfile.php [mode=html|stdout]' . PHP_EOL;
 	exit;
+}
+$mode = DEFAULT_OUTPUT_TYPE;
+if (!empty($_SERVER['argv'][2]) && strtolower($_SERVER['argv'][2]) === 'html') {
+	$mode = 'html';
 }
 
 $test_script_name = realpath($_SERVER['argv'][1]);
@@ -22,16 +27,40 @@ if (!count($status['good'])) {
 	exit;
 }
 
+$text = array();
 foreach ($status['good'] as $version => $it) {
 
 	$out = run_shell_command(DIR_BUILD_PREFIX . $version . '/bin/php -f ' . $test_script_name);
 
-	echo "Version: {$version}";
-	echo PHP_EOL . '---------------------------------' . PHP_EOL;
 	if ($out['stdout']) {
-		echo $out['stdout'];
+		$text[$version] = $out['stdout'];
 	} else {
-		echo 'No output';
+		$text[$version] = 'No output';
 	}
-	echo PHP_EOL . '---------------------------------' . PHP_EOL;
 }
+
+uksort($text, 'strnatcmp');
+
+// FIXME: print_r() isn't ideal here
+if ($mode === 'stdout') {
+	print_r($text);
+	exit;
+}
+
+// Colours defined in inc/config.php
+$uniques        = array_unique($text);
+$colour_matches = array_combine(array_slice($colours, 0, count($uniques)), $uniques);
+
+// FIXME: Better HTML (tables?)
+echo '<table border="1">', PHP_EOL;
+foreach ($text as $version => $output) {
+	$keys = array_keys($colour_matches, $output, TRUE);
+	if (empty($keys[0])) {
+		echo 'ERROR: Very bad.', PHP_EOL;
+		exit;
+	}
+	$bgcolor = '#' . $keys[0];
+
+	echo '<tr bgcolor="', $bgcolor, '"><td valign="top">', $version, '</td><td>', htmlentities($output, ENT_QUOTES, 'UTF-8'), '</td></tr>', PHP_EOL;
+}
+echo '</table>';
--- inc/config.php
+++ inc/config.php
@@ -27,6 +27,10 @@ define ('VERBOSE', true);
 // Options passed before ./configure (Ex: for LIBS, CFLAGS, etc.)
 define ('PRE_CONFIGURE', '');
 
+// Default output mode (see execute_php.php). Either 'stdout' for simple output, or 'html'
+// Example: ./execute_php.php test.php html > test.html && firefox test.html
+define ('DEFAULT_OUTPUT_TYPE', 'stdout');
+
 // Includes all versions greater than defined values, separated by major version
 // Ex: '4', '5' === all PHP 4's and 5's. '5.2.4' === All 5 versions >= 5.2.4
 $php_versions  = array('5.2.11');
@@ -47,4 +51,13 @@ $config_options = array(
 $config_options_versions = array(
 	'5.2.0+' => '--with-foo',
 	'5.3.1'  => '--disable-bar',
+);
+
+// Colours used in optional HTML output, First to Last.
+// FIXME: Will this be enough? Total fail if not :)
+$colours = array(
+	'66FFD9', '66FF8C', 'FFC929', 'FF8C66', 'EBB000', '8CFF66', 'D9FF66', 'FFD966',
+	'668CFF', '8C66FF', 'D966FF', 'FF66D9', '66D9FF', '295EFF', '003BEB', 'FF668C',
+	'FFFFCC', 'E6FFCC', 'CCFFCC', 'CCFFE6', 'FFE6CC', 'FFFF8F', 'FFFF52', 'CCFFFF',
+	'FFCCCC', '5252FF', 'CCE6FF', 'E6CCFF', 'E0E0E0', 'FFFFFF',
 );
